d8e344a48bb1322ebd13ad52499db353b1e360b8
YUVRAJ SINGH <165543892+YuvrajSingh14581@users.noreply.github.com>
2025-09-26 03:05:19 +0530

    Time: 0 ms (100%), Space: 46.7 MB (59.83%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,18 +1,8 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-         Dictionary<int,int>map=new Dictionary<int,int>();
-        for(int i=0;i<nums.Length;i++){
-           if(map.ContainsKey(nums[i])){
-            map[nums[i]]++;
-           }else{
-            map.Add(nums[i],1);
-           }
-        }
-        foreach(var val in map){
-            if(val.Value==1){
-                return val.Key;
-            }
-        }
-        return -1;
+        int xor=0;
+        foreach(int val in nums){
+            xor^=val;
+        }return xor;
     }
 }
