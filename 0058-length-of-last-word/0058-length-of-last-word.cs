8e423df8f76807dad95d62702040ee82b57a6659
YUVRAJ SINGH <165543892+YuvrajSingh14581@users.noreply.github.com>
2025-09-29 12:34:41 +0530

    Time: 0 ms (100%), Space: 40 MB (36.25%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        int count=0;
+        s=s.Trim();
+        for(int i=s.Length-1;i>=0;i--){
+            if(s[i]!=' '){
+                count++;
+            }else{break;}
+        }return count;
+    }
+}
