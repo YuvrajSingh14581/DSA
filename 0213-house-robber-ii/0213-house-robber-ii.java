37aaa13a12142eb35214808286a7d46e60b79918
YUVRAJ SINGH <165543892+YuvrajSingh14581@users.noreply.github.com>
2026-01-23 05:06:44 +0530

    Time: 0 ms (100%), Space: 42.5 MB (92.5%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        int[] temp1=new int[nums.length-1];
-        int[] temp2=new int[nums.length-1];
-        for(int i=0;i<nums.length-1;i++){
-            temp1[i]=nums[i];
-        }for(int i=1;i<nums.length;i++){
-            temp2[i-1]=nums[i];
-        }
-        return Math.max(maxNonAdj(temp1),maxNonAdj(temp2));
+        if(nums.length==1)return nums[0];
+        int ans=Math.max(robbing(nums,0,nums.length-1),robbing(nums,1,nums.length));
+        return ans;
     }
-    public static int maxNonAdj(int[] nums){
-        int prev=nums[0];
+    public static int robbing(int[] nums, int start, int end){
+        int prev1=0;
         int prev2=0;
-        for(int i=1;i<nums.length;i++){
-            int take=nums[i];
-            if(i>1)take+=prev2;
-            int nottake=0+prev;
-            int curi=Math.max(take,nottake);
-            prev2=prev;
-            prev=curi;
-        }
-        return prev;
+        for(int i=start;i<end;i++){
+            int pick=nums[i]+prev2;
+            int notpick=prev1;
+            int curr=Math.max(pick,notpick);
+            prev2=prev1;
+            prev1=curr;
+        }return prev1;
     }
 }
